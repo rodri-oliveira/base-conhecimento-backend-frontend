c73284157713f0f429c325a2712d703f0b5c48e9
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2022-03-05 16:03:51 -0300

    retomando o projeto, configurando knex, criando as migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,43 +2,20 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'postgresql',
+  connection: {
+    database: 'backend_jsl',
+    user: 'postgres',
+    password: '123'
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
+
 
 };
