e784998a613c812e36a10c288161e5232a5309d7
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2022-03-06 11:45:00 -0300

    implementando users.js, com erro no metodo post, corrigindo erro...

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,7 +1,62 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('user save')
+    const { existOrError, notExistOrError, equalsOrError } = app.api.validator
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
     }
 
-    return { save }
+    const save = async (req, res) => {
+        const user = { ...req.body }
+        if(req.params.id) user.id = req.params.id
+
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
+        try {
+            existOrError(user.name, 'Nome não informado')
+            existOrError(user.email, 'E-mail não informado')
+            existOrError(user.senha, 'Senha não informada')
+            existOrError(user.confirmSenha, 'Confirmação de Senha inválida')
+            equalsOrError(user.senha, user.confirmSenha,
+                'Senhas não conferem')
+
+            const userFromDB = await app.db('users')
+                .where({ email: user.email }).first()
+            if(!user.id) {
+                notExistOrError(userFromDB, 'Usuário já cadastrado')
+            }
+        } catch(msg) {
+            return res.status(400).send(msg)
+        }
+
+        user.password = encryptPassword(user.password)
+        delete user.confirmSenha
+
+        if(user.id) {
+            app.db('users')
+                .update(user)
+                .where({ id: user.id })
+                .whereNull('deletedAt')
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('users')
+                .insert(user)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const get = (req, res) => {
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            .whereNull('deletedAt')
+            .then(users => res.json(users))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, get }
 }
--- backend/migrations/20220305154627_create_table_users.js
+++ backend/migrations/20220305154627_create_table_users.js
@@ -1,9 +1,10 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('users', table => {
         table.increments('id').primary()
-        table.string('nome').notNull()
+        table.string('name').notNull()
         table.string('email').unique()
         table.string('senha').notNull()
+        table.string('confirmSenha').notNull()
         table.boolean('admin').notNull().defaultTo(false)
     })
 };
